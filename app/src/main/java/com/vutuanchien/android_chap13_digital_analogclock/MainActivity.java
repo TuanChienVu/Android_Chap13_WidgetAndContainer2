336d19182bfd209119865c306a99b9d0080957c9
VuTuanChien <jbtuanchien.11@gmail.com>
2016-03-23 12:20:50 +0700

    Android_Chap13_Digital_AnalogClock
    Android_Chap13_Digital_AnalogClock

---
+++ app/src/main/java/com/vutuanchien/android_chap13_digital_analogclock/MainActivity.java
@@ -0,0 +1,14 @@
+package com.vutuanchien.android_chap13_digital_analogclock;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+    }
+}
