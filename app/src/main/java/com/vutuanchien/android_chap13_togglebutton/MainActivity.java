ff4d6c17594a8dacb39a47a53b0b0ae04f8858b1
VuTuanChien <jbtuanchien.11@gmail.com>
2016-03-23 12:15:16 +0700

    Android_Chap13_ToggleButton
    Android_Chap13_ToggleButton

---
+++ app/src/main/java/com/vutuanchien/android_chap13_togglebutton/MainActivity.java
@@ -0,0 +1,29 @@
+package com.vutuanchien.android_chap13_togglebutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.CompoundButton;
+import android.widget.ImageView;
+import android.widget.ToggleButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView igLight;
+    ToggleButton toggleButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        igLight = (ImageView) findViewById(R.id.igLight);
+        toggleButton = (ToggleButton) findViewById(R.id.toggleButton);
+
+        toggleButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                toggleButton.setChecked(isChecked);
+                igLight.setImageResource(isChecked ? R.drawable.lighton : R.drawable.lightoff);
+            }
+        });
+    }
+}
